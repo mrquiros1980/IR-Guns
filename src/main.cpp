46e66c074fdc9a8db3f29bda753deb6dc995c760
mrquiros1980 <32739149+mrquiros1980@users.noreply.github.com>
2020-06-11 17:08:55 +0200

    more coding

--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,77 @@
 #include <Arduino.h>
 #include "IRremote.h"
 #include "CppList.h"
+#include <Servo.h>
 
 bool _initialized = false;
 
 int rcv_count;
 IRrecv **all_rcv;
 
+const byte servo1Pin = 9;
+const byte servo2Pin = 10;
+
+Servo servo1; // Crea objeto Servo
+Servo servo2;
+
+unsigned long now1;
+unsigned long now2;
+
+const int interval = 2000;
+
 void setup() {
   if (_initialized) return;
   
   Serial.begin(9600);
   
-  rcv_count = 3;
+  rcv_count = 2;
   all_rcv = (IRrecv **)malloc(rcv_count*sizeof(int));
-  all_rcv[0] = new IRrecv(2);
-  all_rcv[1] = new IRrecv(3);
-  all_rcv[2] = new IRrecv(4);
+  all_rcv[0] = new IRrecv(5);
+  all_rcv[1] = new IRrecv(6);
   
   for (int i=0; i<rcv_count; ++i){
     all_rcv[i]->enableIRIn();
   }
   
+  pinMode(servo1Pin, OUTPUT);
+  pinMode(servo2Pin, OUTPUT);
+
+  servo1.attach(servo1Pin);
+  servo1.write(0);
+  servo2.attach(servo2Pin);
+  servo2.write(0);
+
   _initialized = true;
 }
 
 void loop() {
-  for (int i=0; i<rcv_count; ++i){
-    decode_results results;
-    if (all_rcv[i]->decode(&results)) {
-      int btn = DecodeButton(results.value);
-      Serial.print("Rcv_");
-      Serial.print(i);
-      Serial.print(":");
-      Serial.println(btn);
-      all_rcv[i]->resume();
-    }
+  
+  decode_results results;
+  if (all_rcv[0]->decode(&results)) {
+      
+      Serial.println("Impacto en 1!!");
+      all_rcv[0]->resume();
+      servo1.write(180);
+      now1 = millis();
   }
+
+  if (all_rcv[1]->decode(&results)) {
+      Serial.println("Impacto en 2!!");
+      all_rcv[1]->resume();
+      servo2.write(180);
+      now2 = millis();
+  }
+
+  if((millis() - now1) > interval){
+      servo1.write(0);
+    }
+
+    if((millis() - now2) > interval){
+      servo2.write(0);
+    }
 }
 
-const int BTN_EMPTY = 0;
-const int BTN_POWER = 99;
-const int BTN_0 = 10;
-const int BTN_1 = 1;
-const int BTN_2 = 2;
-const int BTN_3 = 3;
-const int BTN_4 = 4;
-const int BTN_5 = 5;
-const int BTN_STOP = 6;
-const int BTN_DOWN = 7;
-const int BTN_UP = 8;
-const int BTN_BACK = 9;
-const int BTN_FWD = 11;
-const int BTN_EQ = 12;
-const int BTN_REPEAT = 100;
-const int BTN_UNKNOWN = 101;
-const int BTN_ERROR = 102;
 
-int DecodeButton(int param){
-	int rez = BTN_UNKNOWN;
-	switch (param){
-	case 0x0: {
-						rez = BTN_ERROR;
-						break;
-			  }
-	case 0x00FD00FF:
-	case 0x00FF728D:
-	case 0x20DF4EB1: {
-						rez = BTN_POWER;
-						break;
-					 }
-	case 0x00FD30CF:{
-						rez = BTN_0;
-						break;
-					}
-	case 0x00FD08F7:{
-						rez = BTN_1;
-						break;
-					}
-	case 0x00FD8877:{
-						rez = BTN_2;
-						break;
-					}
-	case 0x00FD48B7:{
-						rez = BTN_3;
-						break;
-					}
-	case 0x00FD28D7:{
-						rez = BTN_4;
-						break;
-					}
-	case 0x00FDA857:{
-						rez = BTN_5;
-						break;
-					}
-	case 0x00FD40BF:{
-						rez = BTN_STOP;
-						break;
-					}
-	case 0x00FD10EF:{
-						rez = BTN_DOWN;
-						break;
-					}
-	case 0x00FD50AF:{
-						rez = BTN_UP;
-						break;
-					}
-	case 0x00FD20DF:{
-						rez = BTN_BACK;
-						break;
-					}
-	case 0x00FD609F:{
-						rez = BTN_FWD;
-						break;
-					}
-	case 0x00FDB04F:{
-						rez = BTN_EQ;
-						break;
-					}
-	case 0xFFFFFFFF:{
-						rez = BTN_REPEAT;
-						break;
-					}
-	}
-	return rez;
-}
 
 
 /*
@@ -133,14 +80,6 @@ int DecodeButton(int param){
 
 const byte servo1Pin = 9;
 const byte servo2Pin = 10;
-const int RECV_PIN1 = 5;
-const int RECV_PIN2 = 6;
-
-IRrecv irrecv1(RECV_PIN1);
-IRrecv irrecv2(RECV_PIN2);
-decode_results results1;
-decode_results results2;
-
 Servo servo1; // Crea objeto Servo
 Servo servo2;
 
@@ -152,16 +91,12 @@ int interval = 2000;
 void setup(){
   pinMode(servo1Pin, OUTPUT);
   pinMode(servo2Pin, OUTPUT);
-  pinMode(RECV_PIN1, INPUT);
-  pinMode(RECV_PIN2, INPUT);
 
   servo1.attach(servo1Pin);
   servo1.write(0);
   servo2.attach(servo2Pin);
   servo2.write(0);
   Serial.begin(9600);
-  irrecv1.enableIRIn();
-  irrecv2.enableIRIn();
 }
 
 void loop(){
